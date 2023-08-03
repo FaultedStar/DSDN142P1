a08215580887eedd60fe2736a8ee1dea55478424
Phoebe Zeller <phoebe.zeller@hotmail.com>
2023-08-04 10:29:13 +1200

    final  (not really)

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,12 +1,14 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
-
+let leafx = 100;
+let leafy = 100;
+let leafS = 50;
+ // update colour on line 24  to 
+let isBlue = false;
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -15,9 +17,33 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(220, 255, 220); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  let Green = color(0, 150, 0)
+  let blue = color(0, 0, 150)
+
+  if (isBlue){
+  drawLeaf(leafx, leafy, leafS,blue); // Draw a green leaf at the center
+  }
+  else{
+    drawLeaf(leafx, leafy, leafS,Green);
+  }
+}
+
+function drawLeaf(x, y, leafSize, leafColor) {
+  // Draw the main leaf shape
+  fill(leafColor);
+  noStroke();
+  beginShape();
+  vertex(x, y);
+  bezierVertex(x + leafSize * 0.8, y - leafSize * 0.5, x + leafSize * 0.2, y - leafSize * 0.5, x, y);
+  bezierVertex(x - leafSize * 0.2, y + leafSize * 0.5, x - leafSize * 0.8, y - leafSize * 0.2, x, y);
+  endShape(CLOSE);
+
+  // Draw the stem
+  stroke(leafColor);
+  strokeWeight(5);
+  line(x, y, x, y + leafSize);
 }
